d337d2203883cc9019b72f4fd9a7c0c4ce15a0db
Jonatan Troche <trochealmediajonatan@gmail.com>
2025-06-01 05:20:13 -0400

    add swagger to the project

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const mongodb = require('./data/database')
+const mongodb = require('./data/database');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,6 +10,7 @@ app.use(express.json());
 
 app.use('/', require('./routes'));
 app.use('/members', require('./routes/members'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err) => {
     if (err) {
